e7201d57996cd9bbb23cc772b1dc3ddf6d51aaf2
songjaeheon <spes2you@gmail.com>
2024-03-10 16:58:10 +0900

    feat: Spring Initializr

---
+++ src/main/java/com/footballmania/waslogin/WasLoginApplication.java
@@ -0,0 +1,13 @@
+package com.footballmania.waslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasLoginApplication.class, args);
+	}
+
+}
